7b1eba85f9cf7b3b2faccfc56d2100bf04ce2b3e
Won Sung Hyun <stargazers737@gmail.com>
2022-02-05 23:30:46 +0900

    Lecture1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,17 +9,14 @@ import SwiftUI
 
 struct ContentView: View { // This structure acts as View
     var body: some View { // why Some? why View? type something acts like view
-        return ZStack(content: { // A bag.
+        ZStack{ // A bag.
             RoundedRectangle(cornerRadius: 20.0)
                 .stroke(lineWidth: 3)
-                .padding(.horizontal)
-                .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
-
             Text("Hello, world!")
-                .foregroundColor(.blue)
-                .padding()
-    
-        })
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+
         
     }
 }
