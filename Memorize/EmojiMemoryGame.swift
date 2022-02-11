6c1d7698582c67a290716c5b218fb97505439f83
Won Sung Hyun <stargazers737@gmail.com>
2022-02-12 01:07:42 +0900

    Review the lecture, added some explanation with comments

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 
-
 class EmojiMemoryGame{
     
     // Created once, global -> With static
     static let emojis: [String] = ["⚽️","🏀","🏈","⚾️","🎱","🏓","🥋","🎸","🎲","🎮","🧩"]
+    
     // Created once,
-    static func createMemoryGame() -> MemoryGame<String>{
-        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in // here, pairIndex is like arg of function after IN : emojis[pariIdx]
             emojis[pairIndex]
         }
     }
     
     // the generic type is now a String. private(set). Accessable, but Unchangable. Every Instance
+    // For Static Func (and initializer), need not to read like EmojiMemoryGame.createMemoryGame()
     private var model: MemoryGame<String> = createMemoryGame()
     
     
