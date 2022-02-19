dd543a11c9d76acd2adfc2418a2f7f1ce5addf24
edd3173 <edd3173@github.com>
2022-02-19 15:12:19 +0900

    testCommit

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,38 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by 원성현 on 2022/02/19.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content)-> some View {
+        ZStack{ // A bag.
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            
+            if(isFaceUp) {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                content
+            }
+            else{
+               shape.fill()
+            }
+        }
+    }
+    
+    private struct DrawingConstants{
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+}
+
+extension View{
+    func cardify(isFacedUp:Bool) -> some View{
+        return self.modifier(Cardify(isFaceUp: isFacedUp))
+    }
+}
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -44,22 +44,10 @@ struct CardView: View {
     var body: some View {
         GeometryReader{ geometry in
             ZStack{ // A bag.
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                
-                if(card.isFaceUp) {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Pie(startAngle: Angle(degrees: 0-90), endAngle:Angle(degrees: 110-90)).padding(7).opacity(0.5)
-                    Text(card.content).font(font(in: geometry.size))
-                    
-                }
-                else if card.isMatched{
-                    shape.opacity(0)
-                }
-                else{
-                   shape.fill()
-                }
-            }    
+                Pie(startAngle: Angle(degrees: 0-90), endAngle:Angle(degrees: 110-90)).padding(7).opacity(0.5)
+                Text(card.content).font(font(in: geometry.size))
+            }
+            .cardify(isFacedUp: card.isFaceUp)
         }
     }
     
@@ -68,8 +56,6 @@ struct CardView: View {
     }
     
     private struct DrawingConstants{
-        static let cornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.7
     }
     
